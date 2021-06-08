677287a0b062303677fc76ee84e4fbfd5b5aed2a
fevvelasquez <fevvelasquez@gmail.com>
2021-06-07 19:56:10 -0500

    0.0.3. Create user, @RequestBody
    Implement POST method "/users" endpoint to create a User.
    
    Send User info in JSON format:
    e.g.
    	{
    	"avatar":"https://example/128.jpg",
    	"name": "Juan"
    	}

--- src/main/java/me/fevvelasquez/spring/rest/quicknotes/controllers/UserController.java
+++ src/main/java/me/fevvelasquez/spring/rest/quicknotes/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +18,7 @@ import me.fevvelasquez.spring.rest.quicknotes.services.UserService;
 /**
  * UserController
  * 
- * @version 0.0.2. Get User by Id, @PathVariable
+ * @version 0.0.3. Create user, @RequestBody
  * @author fevvelasquez@gmail.com
  *
  */
@@ -35,6 +37,18 @@ public class UserController {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> getUserbyId(@PathVariable("id") Integer id) {
 		return new ResponseEntity<User>(userService.getUserById(id), HttpStatus.OK);
+	}
+
+	/**
+	 * Creates a User.
+	 * 
+	 * @param user in JSON format. 
+	 * 			e.g. { "avatar":"https://example/128.jpg","name": "Juan" }
+	 * @return new user with id info.
+	 */
+	@PostMapping
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		return new ResponseEntity<User>(userService.createUser(user), HttpStatus.CREATED);
 	}
 
 }
